7386e79c6638157cfdeb33308dadbe2d1661ea5d
JamesEArnold <jearnold22@gmail.com>
2023-08-08 19:53:46 -0400

    yodaleyheyhoooooooo

--- components/main-accordion.tsx
+++ components/main-accordion.tsx
@@ -4,28 +4,44 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheckToSlot, faClipboard, faHandshake } from '@fortawesome/free-solid-svg-icons'
 
 export function MainAccordion() {
   return (
     <div className="px-3">
       <Accordion type="single" collapsible className="w-full">
-        <AccordionItem value="item-1" className="bg-yellow-500">
-          <AccordionTrigger className="justify-center">Priorities</AccordionTrigger>
+        <AccordionItem value="item-1" className="bg-yellow-400">
+          <AccordionTrigger className="justify-center uppercase font-bold">
+            <span>
+              <FontAwesomeIcon icon={faCheckToSlot} className="px-3"/> 
+              Priorities
+            </span>
+          </AccordionTrigger>
           <AccordionContent className="px-3">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
           </AccordionContent>
         </AccordionItem>
-        <AccordionItem value="item-2">
-          <AccordionTrigger>Is it styled?</AccordionTrigger>
-          <AccordionContent>
-            Yes. It comes with default styles that matches the other
-            components&apos; aesthetic.
-          </AccordionContent>
+        <AccordionItem value="item-2" className="bg-yellow-500">
+            <AccordionTrigger className="justify-center uppercase font-bold">
+              <span>
+                <FontAwesomeIcon icon={faClipboard} style={{color: "#000",}} className="px-3"/> 
+                Volunteer 
+              </span>
+            </AccordionTrigger>
+            <AccordionContent>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </AccordionContent>
         </AccordionItem>
-        <AccordionItem value="item-3">
-          <AccordionTrigger>Is it animated?</AccordionTrigger>
+        <AccordionItem value="item-3" className="bg-yellow-600">
+          <AccordionTrigger className="justify-center uppercase font-bold">
+            <span>
+              <FontAwesomeIcon icon={faHandshake} className="px-3"/> 
+              Contribute
+            </span>
+          </AccordionTrigger>
           <AccordionContent>
-            Yes. It's animated by default, but you can disable it if you prefer.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
           </AccordionContent>
         </AccordionItem>
       </Accordion>
