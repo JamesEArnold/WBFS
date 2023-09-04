e1d36c677c2408c777643a2fca1c0a97ac82288a
JamesEArnold <jearnold22@gmail.com>
2023-09-03 23:13:18 -0400

    swimmy swimmy goldfish

---
+++ components/seo/globalMeta.ts
@@ -0,0 +1,7 @@
+export const globalMeta = {
+  siteName: 'Weisburn For Sheriff',
+  siteUrl: 'https://weisburnforsheriff.com',
+  siteLogo: 'some_site_logo',
+  email: 'weisburnforsheriff@gmail.com',
+  description: 'The official site for the Stark County Republican Sheriff Representative Eric Weisburn.'
+}
