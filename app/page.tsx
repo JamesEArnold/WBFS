1db0530eb881092c4283db19657940d4f8387216
JamesEArnold <jearnold22@gmail.com>
2023-09-03 21:27:20 -0400

    whooooooooop

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,27 @@
 
 import { HeroSlide } from '@/components/hero-slide';
 import { MainNav } from '@/components/main-nav'
-import { CallToAction } from '@/components/call-to-action';
+import { CallToActionBottom, CallToActionTop } from '@/components/call-to-action';
 import { MainAccordion } from '@/components/main-accordion';
+import { AboutMe } from '@/components/about-me';
+import { Carousel } from '@/components/carousel';
+import { Footer } from '@/components/footer';
 
 export default function Home() {
   return (
-      <main className="flex flex-col md:mx-auto md:max-w-7xl">
+    <>
+      <header className="top-0 sticky z-50 shadow-md">
         <MainNav />
+      </header>
+      <main className="relative flex flex-col md:mx-auto md:max-w-7xl">
         <HeroSlide />
-        <CallToAction />
+        <CallToActionTop />
         <MainAccordion />
+        <AboutMe />
+        <Carousel />
+        <CallToActionBottom />
+        <Footer />
       </main>
+    </>
   )
 }
---
+++ components/footer.tsx
@@ -0,0 +1,46 @@
+import { Button } from "./ui/button";
+import Image from "next/image";
+
+export const Footer = () => {
+  return <div className="bg-yellow-400">
+    <div className="flex flex-col w-10/12 mx-auto mt-6">
+      <Button className="uppercase p-8 font-bold text-lg drop-shadow-lg rounded-md my-2">Volunteer</Button>
+      <Button className="uppercase p-8 font-bold text-lg drop-shadow-lg rounded-md">Contribute</Button>
+      <hr className="h-px my-8 bg-gray-800 opacity-25 border-0 dark:bg-gray-700"></hr>
+    </div>
+    <div className="flex flex-col mx-auto w-full h-full mt-6">
+      <Image
+          priority
+          src="/logo.svg"
+          alt="Weisburn For Sheriff"
+          height={150}
+          width={380}
+          className="mx-auto"
+        />
+        <div className="flex w-8/12 uppercase mx-auto border-2 border-black opacity-40 my-2 rounded">
+          <span className="mx-auto py-3 text-[11px] opacity-100">Paid for by Weisburn for Sheriff</span>
+        </div>
+        <div className="flex flex-col w-8/12 mx-auto text-sm opacity-60 mt-4">
+          <span className="mx-auto">123 Fake Street Canton, OH 44701</span>
+          <span className="mx-auto"> weisburnforsheriff@gmail.com</span>
+        </div>
+        <div className="flex flex-center justify-center mt-4 mb-20">
+          { /* TODO: Make these buttons, and then actually link them to the socials */}
+          <Image
+            src="/icon-facebook.svg"
+            width="50"
+            height="50"
+            alt="Weisburn for Sheriff Facebook"
+            className="mx-2"
+          />
+          <Image
+            src="/icon-twitter.png"
+            width="50"
+            height="50"
+            alt="Weisburn for Sheriff Twitter"
+            className="mx-2"
+          />
+        </div>
+    </div>
+  </div>
+};
