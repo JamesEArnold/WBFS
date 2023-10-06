f86fe85466b5bf9a99daade45da178513ceb48f3
JamesEArnold <jearnold22@gmail.com>
2023-10-06 19:37:45 -0400

    add side padding to accordion text

--- components/main-accordion.tsx
+++ components/main-accordion.tsx
@@ -12,7 +12,7 @@ export function MainAccordion() {
     <div className="px-3">
       <Accordion type="single" collapsible className="w-full">
         <AccordionItem value="item-1" className="bg-yellow-400">
-          <AccordionTrigger className="justify-center uppercase font-bold">
+          <AccordionTrigger className="justify-center font-bold uppercase">
             <span>
               <FontAwesomeIcon icon={faCheckToSlot} className="px-3"/> 
               Priorities
@@ -23,24 +23,24 @@ export function MainAccordion() {
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-2" className="bg-yellow-500">
-            <AccordionTrigger className="justify-center uppercase font-bold">
+            <AccordionTrigger className="justify-center font-bold uppercase">
               <span>
                 <FontAwesomeIcon icon={faClipboard} style={{color: "#000",}} className="px-3"/> 
                 Volunteer 
               </span>
             </AccordionTrigger>
-            <AccordionContent>
+            <AccordionContent className="px-3">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-3" className="bg-yellow-600">
-          <AccordionTrigger className="justify-center uppercase font-bold">
+          <AccordionTrigger className="justify-center font-bold uppercase">
             <span>
               <FontAwesomeIcon icon={faHandshake} className="px-3"/> 
               Contribute
             </span>
           </AccordionTrigger>
-          <AccordionContent>
+          <AccordionContent className="px-3">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
           </AccordionContent>
         </AccordionItem>
