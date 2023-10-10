654e6e21e4512b8328ab5f1fbc8c9afd1a756ea7
JamesEArnold <jearnold22@gmail.com>
2023-10-10 15:32:12 -0400

    fix bad html tags i guess

--- app/donate/page.tsx
+++ app/donate/page.tsx
@@ -19,9 +19,9 @@ export default function Home() {
         <div id="donation-form"></div>
       </main> */}
       <main>
-      <script src="https://donorbox.org/widget.js" paypalExpress="false"></script>
+      <script src="https://donorbox.org/widget.js" data-paypalExpress="false"></script>
       <div className="flex justify-center my-10">
-        <iframe src="https://donorbox.org/embed/weisburn-for-sheriff" name="donorbox" allowpaymentrequest="allowpaymentrequest" seamless={true} frameborder="0" scrolling="no" height="900px" width="100%" className="max-w-[500px] min-w-[250px] max-h-none"></iframe>
+        <iframe src="https://donorbox.org/embed/weisburn-for-sheriff" name="donorbox" data-allowpaymentrequest="allowpaymentrequest" seamless={true} data-frameborder="0" scrolling="no" height="900px" width="100%" className="max-w-[500px] min-w-[250px] max-h-none"></iframe>
         <div className="relative hidden w-1/3 my-auto mr-3 overflow-hidden md:inline-block">
           <Image
             src="/ElectWeisburnForSheriff.png"
