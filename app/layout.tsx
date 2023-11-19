da48c75cb309db3e912ed18db83de76aba3b8bfd
JamesEArnold <jearnold22@gmail.com>
2023-11-18 22:44:44 -0500

    add google search verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,7 @@ export default function RootLayout({
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        <meta name="google-site-verification" content="9J5nLPwwmIYa-i-ryNRJ10p0pB0wFeXk9IFXk5Ff1Mw" />
       </Head>
       <body className={`${inter.className}`}>
       <Meta 
