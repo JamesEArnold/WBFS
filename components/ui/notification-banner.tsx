aa1d86d66e3e0e1887031646fd71168e5719aeb4
JamesEArnold <jearnold22@gmail.com>
2024-04-18 21:02:07 -0400

    add golf fundraiser to banner

--- components/ui/notification-banner.tsx
+++ components/ui/notification-banner.tsx
@@ -2,30 +2,34 @@ import './notification-banner.css';
 
 export const NotificationBanner = () => {
   return (
-    // <div className="py-2 font-bold text-center text-white uppercase bg-black">
-    //   <span>Upcoming events: Campaign Kickoff Event</span>
-    //   <br />
-    //   <a href="#" className="text-yellow-300 underline">Reserve your spot now!</a>
-    // </div>
-    <div className="ticker-wrap">
-      <div className="ticker">
-        <div className="font-bold text-white uppercase ticker__item">
-          <span>Follow us on&nbsp;
-            <a 
-              className="text-yellow-300 underline" 
-              href="https://www.facebook.com/people/Weisburn-For-Sheriff-2024/61552050077933/"
-            >
-              Facebook
-            </a> and&nbsp;
-            <a
-              className="text-yellow-300 underline"
-              href="https://www.instagram.com/weisburnforsheriff"
-            >
-              Instagram!
-            </a>
-          </span>
+<div className="ticker-wrap">
+        <div className="ticker">
+          <div className="font-bold text-white uppercase ticker__item">
+            <span>
+              <span className="text-yellow-300">Upcoming events:</span>
+              &nbsp;Golf Fundraiser&nbsp;
+              <span className="text-yellow-300">|</span> 
+              &nbsp;August 23rd at Arrowhead Golf Club
+            </span>
+            <br />
+          </div>
+          <div className="font-bold text-white uppercase ticker__item">
+            <span>Follow us on&nbsp;
+              <a
+                className="text-yellow-300 underline"
+                href="https://www.facebook.com/people/Weisburn-For-Sheriff-2024/61552050077933/"
+              >
+                Facebook
+              </a> and&nbsp;
+              <a
+                className="text-yellow-300 underline"
+                href="https://www.instagram.com/weisburnforsheriff"
+              >
+                Instagram!
+              </a>
+            </span>
+          </div>
         </div>
       </div>
-    </div>
   );
 }
